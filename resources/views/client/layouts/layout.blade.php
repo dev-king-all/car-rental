b077e6a858030a6990389a46c24ba1b1abad4a1d
jokeronami <onamimartin@gmail.com>
2021-07-07 09:52:51 +0300

    Favicon | Logos | Titles | User Card | Dark Mode
    --Updated Favicon
    --Updated Title
    --Updated User Card
    --Updated Dark Mode Menu

--- resources/views/client/layouts/layout.blade.php
+++ resources/views/client/layouts/layout.blade.php
@@ -8,9 +8,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="A powerful and conceptual apps base dashboard template that especially build for developers and programmers.">
     <!-- Fav Icon  -->
-    <link rel="shortcut icon" href="./images/favicon.png">
+        <link rel="shortcut icon" href="{{ asset('./admin/images/favicon.png ') }}">
+
     <!-- Page Title  -->
-    <title>Invest Dashboard | DashLite Admin Template</title>
+    <title>xFusion App | Client</title>
     <!-- StyleSheets  -->
     <link rel="stylesheet" href="{{ asset('agent/css/dashlite.css?ver=2.4.0') }}">
     <link id="skin-default" rel="stylesheet" href="{{ asset('agent/css/theme.css?ver=2.4.0') }}">
@@ -29,22 +30,12 @@
                     </div>
                     <div class="nk-header-brand">
                         <a href="html/index.html" class="logo-link">
-                            <img class="logo-light logo-img" src="{{ asset('agent/images/logo.png') }}" srcset="./images/logo2x.png 2x" alt="logo">
-                            <img class="logo-dark logo-img" src="{{ asset('agent/images/logo-dark.png') }}" srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
+                            <img class="logo-light logo-img" src="{{ asset('./admin/images/logo.png') }}" srcset="{{ asset('./admin/images/logo2x.png 2x') }}" alt="logo">
+                            <img class="logo-dark logo-img" src="{{ asset('./admin/images/logo-dark.png') }}" srcset="{{ asset('./admin/images/logo-dark2x.png 2x') }}" alt="logo-dark">
                         </a>
                     </div><!-- .nk-header-brand -->
                     <div class="nk-header-menu" data-content="headerNav">
-                        <div class="nk-header-mobile">
-                            <div class="nk-header-brand">
-                                <a href="html/index.html" class="logo-link">
-                                    <img class="logo-light logo-img" src="{{ asset('agent/images/logo.png') }}" srcset="./images/logo2x.png 2x" alt="logo">
-                                    <img class="logo-dark logo-img" src="{{ asset('agent/images/logo-dark.png') }}" srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
-                                </a>
-                            </div>
-                            <div class="nk-menu-trigger mr-n2">
-                                <a href="#" class="nk-nav-toggle nk-quick-nav-icon" data-target="headerNav"><em class="icon ni ni-arrow-left"></em></a>
-                            </div>
-                        </div>
+
                         <ul class="nk-menu nk-menu-main ui-s2">
                             <li class="nk-menu-item has-sub">
                                 <a href="#" class="nk-menu-link nk-menu-toggle">
@@ -570,34 +561,22 @@
                                     </div>
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-md dropdown-menu-right dropdown-menu-s1 is-light">
-                                    <div class="dropdown-inner user-card-wrap bg-lighter d-none d-md-block">
-                                        <div class="user-card">
-                                            <div class="user-avatar">
-                                                <span>AB</span>
-                                            </div>
-                                            @if(Auth::user())
-                                                <div class="user-info">
-                                                    <span class="lead-text">{{ Auth::user()->name }}</span>
-                                                    <span class="sub-text">{{ Auth::user()->email }}</span>
-                                                </div>
-                                            @endif
-                                            <div class="user-action">
-                                                <a class="btn btn-icon mr-n2" href="html/invest/profile-setting.html"><em class="icon ni ni-setting"></em></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="dropdown-inner user-account-info">
-                                        <h6 class="overline-title-alt">Account Balance</h6>
-                                        <div class="user-balance">1,494.23 <small class="currency currency-usd">USD</small></div>
-                                        <div class="user-balance-sub">Locked <span>15,495.39 <span class="currency currency-usd">USD</span></span></div>
-                                        <a href="#" class="link"><span>Withdraw Balance</span> <em class="icon ni ni-wallet-out"></em></a>
-                                    </div>
+
                                     <div class="dropdown-inner">
                                         <ul class="link-list">
                                             <li><a href="html/invest/profile.html"><em class="icon ni ni-user-alt"></em><span>View Profile</span></a></li>
                                             <li><a href="html/invest/profile-setting.html"><em class="icon ni ni-setting-alt"></em><span>Account Setting</span></a></li>
                                             <li><a href="html/invest/profile-activity.html"><em class="icon ni ni-activity-alt"></em><span>Login Activity</span></a></li>
-                                            <li><a class="dark-mode-switch" href="#"><em class="icon ni ni-moon"></em><span>Dark Mode</span></a></li>
+                                            <li><a class="dark-switch" href="#"><em class="icon ni ni-moon"></em><span>Dark Mode</span></a></li>
+                                            <li><a class="dropdown-item" href="{{ route('logout') }}"
+                                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                                    <em class="icon ni ni-signout"></em><span>  {{ __('Sign out') }}</span>
+                                                </a>
+                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                    @csrf
+                                                </form>
+                                            </li>
                                         </ul>
                                     </div>
                                     <div class="dropdown-inner">
