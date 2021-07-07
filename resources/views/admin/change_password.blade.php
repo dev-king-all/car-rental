f877ed242bda531d971c68c94b62d6ecc25122e3
jokeronami <onamimartin@gmail.com>
2021-07-07 10:01:14 +0300

    Prettified HTML
    HTMl blocks were not properly indented.

--- resources/views/admin/change_password.blade.php
+++ resources/views/admin/change_password.blade.php
@@ -20,10 +20,9 @@
                                         <div class="nk-block-des"><p>These settings are helps you keep your account
                                                 secure.</p></div>
                                     </div>
-                                    <div class="nk-block-head-content align-self-start d-lg-none"><a href="#"
-                                                                                                     class="toggle btn btn-icon btn-trigger mt-n1"
-                                                                                                     data-target="userAside"><em
-                                                    class="icon ni ni-menu-alt-r"></em></a></div>
+                                    <div class="nk-block-head-content align-self-start d-lg-none"><a href="#" class="toggle btn btn-icon btn-trigger mt-n1"
+                                     data-target="userAside"><em
+                                     class="icon ni ni-menu-alt-r"></em></a></div>
                                 </div>
                             </div>
                             <div class="nk-block">
