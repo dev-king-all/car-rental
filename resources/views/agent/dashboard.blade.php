f2d52bec997d21aa2735ae366f01969cba9bbf24
jokeronami <onamimartin@gmail.com>
2021-07-07 10:02:11 +0300

    Removed HTML block
    Removed the Dashboard introduction head content.

--- resources/views/agent/dashboard.blade.php
+++ resources/views/agent/dashboard.blade.php
@@ -8,12 +8,6 @@
                         <div class="nk-block-head nk-block-head-sm">
                             <div class="nk-block-between">
                                 <div class="nk-block-head-content">
-                                    <h3 class="nk-block-title page-title">Investment Dashboard</h3>
-                                    <div class="nk-block-des text-soft">
-                                        <p>Welcome to DashLite Dashboard Template.</p>
-                                    </div>
-                                </div><!-- .nk-block-head-content -->
-                                <div class="nk-block-head-content">
                                     <div class="toggle-wrap nk-block-tools-toggle">
                                         <a href="#" class="btn btn-icon btn-trigger toggle-expand mr-n1" data-target="pageMenu"><em class="icon ni ni-more-v"></em></a>
                                         <div class="toggle-expand-content" data-content="pageMenu">
