a7c2e4c88aba8dbc2ccd1b3e669d5ccb6fa2a742
jokeronami <onamimartin@gmail.com>
2021-07-09 21:13:38 +0300

    Updated Password Reset Email

--- resources/views/verify_email.blade.php
+++ resources/views/verify_email.blade.php
@@ -6,7 +6,7 @@
                 <tr style="box-sizing: border-box;">
                     <td class="text-center pb-4" style="box-sizing: border-box; padding-bottom: 1.5rem; text-align: center;" align="center">
                         <a href="#" style="box-sizing: border-box; text-decoration: none; background-color: transparent; transition: color .4s, background-color .4s, border .4s, box-shadow .4s; color: #6576ff; word-break: break-all;"><img class="email-logo" src="https://dashlite.net/demo3/images/logo-dark2x.png" alt="logo" style="box-sizing: border-box; vertical-align: middle; border-style: none; max-width: 100%; height: 40px;" height="40"></a>
-                        <p class="email-title" style="box-sizing: border-box; margin-top: 0; margin-bottom: 0; font-size: 13px; color: #6576ff; padding-top: 12px;">Verification Pending Email</p>
+                        <p class="text-warning" style="box-sizing: border-box; margin-top: 0; margin-bottom: 0; font-size: 13px; color: #6576ff; padding-top: 12px;">Verification Pending Email</p>
                     </td>
                 </tr>
                 </tbody>
@@ -15,7 +15,7 @@
                 <tbody style="box-sizing: border-box;">
                 <tr style="box-sizing: border-box;">
                     <td class="px-3 px-sm-5 pt-3 pt-sm-5 pb-4" style="box-sizing: border-box; padding-top: 1rem; padding-right: 1rem; padding-left: 1rem; padding-bottom: 1.5rem;">
-                        <img class="w-100px" src="https://dashlite.net/demo3/images/email/kyc-progress.png" alt="In Process" style="box-sizing: border-box; vertical-align: middle; border-style: none; max-width: 100%; width: 100px;" width="100">
+                        <img class="w-100px" src="https://dashlite.net/demo3/images/email/kyc-pending.png" alt="In Process" style="box-sizing: border-box; vertical-align: middle; border-style: none; max-width: 100%; width: 100px;" width="100">
                     </td>
                 </tr>
                 <tr style="box-sizing: border-box;">
@@ -23,8 +23,16 @@
                         <p
                                 style="box-sizing: border-box; margin-top: 0; margin-bottom: 1rem;"><strong
                                     style="box-sizing: border-box; font-weight: 500;">Hi {{ $name }},</strong></p>
-                        <h5 class="text-primary mb-3" style="box-sizing: border-box; margin-top: 0; font-family: Roboto,sans-serif; font-weight: 700; line-height: 1.1; font-size: 1.15rem; letter-spacing: -0.01em; margin-bottom: 1rem; color: #6576ff;">Your application under varification process.</h5>
-                        <p style="box-sizing: border-box; margin-top: 0; margin-bottom: 0;">We are still working on your identity verification. Once our team verified your indentity, you will be whitelisted and notified by email.</p>
+                                    <p
+                                style="box-sizing: border-box; margin-top: 0; margin-bottom: 1rem;"><strong
+                                    style="box-sizing: border-box; font-weight: 500;">Thank you for Signing Up for the xFusion Experience :)</strong></p>
+
+                        <h5 class="text-warning mb-3" style="box-sizing: border-box; margin-top: 0; font-family: Roboto,sans-serif; font-weight: 700; line-height: 1.1; font-size: 1.15rem; letter-spacing: -0.01em; margin-bottom: 1rem; color: #f4bd0e;">Your application is Pending Approval.</h5>
+                        <p style="box-sizing: border-box; margin-top: 0; margin-bottom: 0;">We are still working on verifying your Sign Up. This shouldn't take a while, once our team member has verified this you will receieve an email in this regard.</p>
+
+                         </br>
+
+                         <p style="box-sizing: border-box; margin-top: 1; margin-bottom: 0;"><strong>Thank You for your Patience.</strong></p>
                     </td>
                 </tr>
                 </tbody>
@@ -35,10 +43,10 @@
                     <td class="text-center pt-4" style="box-sizing: border-box; padding-top: 1.5rem; text-align: center;" align="center">
                         <p class="email-copyright-text" style="box-sizing: border-box; margin-top: 0; margin-bottom: 1rem; font-size: 13px;">Copyright © 2021 xFusion. All rights reserved. <br style="box-sizing: border-box;">.</p>
                         <ul class="email-social" style="box-sizing: border-box; margin-top: 0; margin-bottom: 1rem; list-style: none; margin: 0; padding: 0;">
-                            <li style="box-sizing: border-box; display: inline-block; padding: 4px;"><a href="#" style="box-sizing: border-box; text-decoration: none; background-color: transparent; transition: color .4s, background-color .4s, border .4s, box-shadow .4s; color: #6576ff; word-break: break-all; display: inline-block; height: 30px; width: 30px; border-radius: 50%; background: #ffffff;"><img src="{{ asset('./admin/images/socials/facebook.png ') }}" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; max-width: 100%; width: 30px;" width="30"></a></li>
-                            <li style="box-sizing: border-box; display: inline-block; padding: 4px;"><a href="#" style="box-sizing: border-box; text-decoration: none; background-color: transparent; transition: color .4s, background-color .4s, border .4s, box-shadow .4s; color: #6576ff; word-break: break-all; display: inline-block; height: 30px; width: 30px; border-radius: 50%; background: #ffffff;"><img src="{{ asset('./admin/images/socials/twitter.png ') }}" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; max-width: 100%; width: 30px;" width="30"></a></li>
-                            <li style="box-sizing: border-box; display: inline-block; padding: 4px;"><a href="#" style="box-sizing: border-box; text-decoration: none; background-color: transparent; transition: color .4s, background-color .4s, border .4s, box-shadow .4s; color: #6576ff; word-break: break-all; display: inline-block; height: 30px; width: 30px; border-radius: 50%; background: #ffffff;"><img src="{{ asset('./admin/images/socials/youtube.png ') }}" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; max-width: 100%; width: 30px;" width="30"></a></li>
-                            <li style="box-sizing: border-box; display: inline-block; padding: 4px;"><a href="#" style="box-sizing: border-box; text-decoration: none; background-color: transparent; transition: color .4s, background-color .4s, border .4s, box-shadow .4s; color: #6576ff; word-break: break-all; display: inline-block; height: 30px; width: 30px; border-radius: 50%; background: #ffffff;"><img src="{{ asset('./admin/images/socials/medium.png ') }}" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; max-width: 100%; width: 30px;" width="30"></a></li>
+                            <li style="box-sizing: border-box; display: inline-block; padding: 4px;"><a href="#" style="box-sizing: border-box; text-decoration: none; background-color: transparent; transition: color .4s, background-color .4s, border .4s, box-shadow .4s; color: #6576ff; word-break: break-all; display: inline-block; height: 30px; width: 30px; border-radius: 50%; background: #ffffff;"><img src="https://dashlite.net/demo3/images/socials/facebook.png" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; max-width: 100%; width: 30px;" width="30"></a></li>
+                            <li style="box-sizing: border-box; display: inline-block; padding: 4px;"><a href="#" style="box-sizing: border-box; text-decoration: none; background-color: transparent; transition: color .4s, background-color .4s, border .4s, box-shadow .4s; color: #6576ff; word-break: break-all; display: inline-block; height: 30px; width: 30px; border-radius: 50%; background: #ffffff;"><img src="https://dashlite.net/demo3/images/socials/twitter.png" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; max-width: 100%; width: 30px;" width="30"></a></li>
+                            <li style="box-sizing: border-box; display: inline-block; padding: 4px;"><a href="#" style="box-sizing: border-box; text-decoration: none; background-color: transparent; transition: color .4s, background-color .4s, border .4s, box-shadow .4s; color: #6576ff; word-break: break-all; display: inline-block; height: 30px; width: 30px; border-radius: 50%; background: #ffffff;"><img src="https://dashlite.net/demo3/images/socials/youtube.png" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; max-width: 100%; width: 30px;" width="30"></a></li>
+                            <li style="box-sizing: border-box; display: inline-block; padding: 4px;"><a href="#" style="box-sizing: border-box; text-decoration: none; background-color: transparent; transition: color .4s, background-color .4s, border .4s, box-shadow .4s; color: #6576ff; word-break: break-all; display: inline-block; height: 30px; width: 30px; border-radius: 50%; background: #ffffff;"><img src="https://dashlite.net/demo3/images/socials/medium.png" alt="" style="box-sizing: border-box; vertical-align: middle; border-style: none; max-width: 100%; width: 30px;" width="30"></a></li>
                         </ul>
                         <p class="fs-12px pt-4" style="box-sizing: border-box; margin-top: 0; margin-bottom: 0; font-size: 12px; padding-top: 1.5rem;">This is an automated email sent to you by the xFusion App <a href="https://xfusion.io" style="box-sizing: border-box; text-decoration: none; background-color: transparent; transition: color .4s, background-color .4s, border .4s, box-shadow .4s; color: #6576ff; word-break: break-all;">xfusion.io</a>. Please do not respond to this Email.</p>
                     </td>
