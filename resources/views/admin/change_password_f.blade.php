4b6d7f7457aa88dc45ece3d112dc0d0c5f3d4157
jokeronami <onamimartin@gmail.com>
2021-07-16 00:37:19 +0300

    Added Text Area Box to Address section in Admin

--- resources/views/admin/change_password_f.blade.php
+++ resources/views/admin/change_password_f.blade.php
@@ -23,34 +23,34 @@
                             </div>
                         @endif
                             @if(Session::has('message'))
-                                <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
+                                <p class="alert {{ Session::get('alert-class', 'alert-primary') }}">{{ Session::get('message') }}</p>
                             @endif
                         <form action="{{ route('admin_c_p') }}" method="post">
                             @csrf
                             <div class="row g-gs">
                                 <div class="col-md-6">
-                                    <div class="form-group"><label class="form-label"
-                                                                   for="password">Password</label>
-                                        <div class="form-control-wrap"><input type="text" class="form-control"
-                                                                              id="password"
-                                                                              name="password" required=""></div>
+                                    <div class="form-group">
+                                        <label class="form-label" for="password">Password</label>
+                                        <div class="form-control-wrap"><input type="password" class="form-control" id="password" name="password" required=""></div>
                                     </div>
                                 </div>
                             </br>
                                 <div class="col-md-6">
-                                    <div class="form-group"><label class="form-label"
-                                                                   for="c_password">Confirm Password</label>
-                                        <div class="form-control-wrap"><input type="text" class="form-control"
-                                                                              id="c_password"
-                                                                              name="c_password" required=""></div>
+                                    <div class="form-group">
+                                        <label class="form-label" for="c_password">Confirm Password</label>
+                                        <div class="form-control-wrap"><input type="password" class="form-control" id="c_password" name="c_password" required=""></div>
                                     </div>
                                 </div>
                                 <div class="col-md-12">
                                     <div class="form-group">
-
-                                        <a href="{{ route('admin.change.password') }}" class="btn btn-info">Back</a>
-                                        <input type="submit" value="Save" class="btn btn-success">
-
+                                      <ul class="align-center flex-wrap flex-sm-nowrap gx-4 gy-2">
+                                        <li>
+                                        <a href="{{ route('admin.change.password') }}" class="link link-light"> Back </a>
+                                        </li>
+                                        <li>
+                                        <input type="submit" value="Save New Password" class="btn btn-lg btn-primary">
+                                        </li>
+                                     </ul>
                                     </div>
                                 </div>
                             </div>
